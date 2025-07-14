ea48c0aa91dfb4ff5c08ed972248a2b8d8487551
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-14 15:38:22 -0700

    rfc8141.NIDs

---
+++ nids.go
@@ -0,0 +1,101 @@
+package rfc8141
+
+// NIDs is a list of registered URN NIDs, taken from:
+//
+// https://www.iana.org/assignments/urn-namespaces/urn-namespaces.xhtml
+//
+// ... when it was last updated on:
+//
+//	2025-07-02
+var NIDs = []string{
+	"3gpp",                // IETF RFC-5279
+	"3gpp2",               // IETF RFC-8464
+	"adid",                // IETF RFC-8107
+	"alert",               // IETF RFC-7462
+	"bbf",                 // IETF RFC-8057
+	"broadband-forum-org", // IETF RFC-8057
+	"c2pa",                // C2PA
+	"cablelabs",           // IETF RFC-6289
+	"ccsds",               // IETF RFC-7738
+	"cdx",                 // The OWASP Foundation Inc.
+	"cgi",                 // IETF RFC-5138
+	"clei",                // IETF RFC-4152
+	"csa",                 // Connectivity Standards Alliance
+	"cta",                 // Consumer Technology Association
+	"ddi",                 // IETF RFC-9517
+	"dev",                 // IETF RFC-9039
+	"dgiwg",               // IETF RFC-6288
+	"doi",                 // DOI Foundation
+	"dslforum-org",        // IETF RFC-8057
+	"dvb",                 // IETF RFC-5328 ; IETF RFC-7354
+	"ebu",                 // IETF RFC-5174
+	"eic",                 // European Network of Transmission System Operators for Electricity (ENTSO-E)
+	"eidr",                // IETF RFC-7972
+	"epc",                 // RFC-5134
+	"epcglobal",           // IETF RFC-5134
+	"etsi",                // IETF RFC-8515
+	"eurosystem",          // IETF RFC-7207
+	"example",             // IETF RFC-6963
+	"fdc",                 // IETF RFC-4198
+	"fipa",                // IETF RFC-3616
+	"gdst",                // Institute of Food Technologists
+	"geant",               // IETF RFC-4926
+	"globus",              // IETF RFC-7853
+	"gsma",                // IETF RFC-7254
+	"gvat",                // Bundeskanzleramt der Republik Österreich
+	"hbbtv",               // IETF RFC-7528
+	"ieee",                // IETF RFC-8069
+	"ietf",                // IETF RFC-2648
+	"iptc",                // IETF RFC-3937
+	"isan",                // IETF RFC-4246
+	"isbn",                // International ISBN Agency
+	"iso",                 // IETF RFC-5141
+	"issn",                // ISSN International Centre
+	"itu",                 // International Telecommunications Union (ITU)
+	"ivis",                // IETF RFC-4617
+	"knx",                 // KNX Association cvba
+	"lei",                 // Global Legal Entity Identifier Foundation (GLEIF)
+	"lex",                 // IETF RFC-9676
+	"liberty",             // IETF RFC-3622
+	"mace",                // IETF RFC-3613
+	"mef",                 // IETF RFC-7818
+	"meta",                // uha_Hakala
+	"mpeg",                // IETF RFC-3614
+	"mrn",                 // International Association of Marine Aids to Navigation and Lighthouse Authorities (IALA)
+	"nan",                 // The National Archives of Finland
+	"nato",                // IETF RFC-7467
+	"nbn",                 // IETF RFC-8458
+	"nena",                // IETF RFC-6061
+	"newsml",              // IETF RFC-3085
+	"nfc",                 // IETF RFC-4729
+	"nzl",                 // IETF RFC-4350
+	"oasis",               // IETF RFC-3121
+	"ogc",                 // IETF RFC-5165
+	"ogf",                 // IETF RFC-6453
+	"oid",                 // IETF RFC-3061
+	"oipf",                // IETF RFC-6893
+	"oma",                 // IETF RFC-4358
+	"onem2m",              // oneM2M Partnership Project
+	"onf",                 // Open Networking Foundation
+	"pin",                 // IETF RFC-3043
+	"pno",                 // PROFIBUS Nutzerorganisation e.V.
+	"publicid",            // IETF RFC-3151
+	"pwid",                // Eld_Zierau
+	"reso",                // Real Estate Standards Organization (RESO)
+	"s1000d",              // IETF RFC-4688
+	"schac",               // IETF RFC-6338
+	"service",             // IETF RFC-5031
+	"smpte",               // IETF RFC-5119
+	"stalwart",            // Stalwart Labs LLC
+	"swift",               // IETF RFC-3615
+	"thread",              // Thread Group, Inc.
+	"tva",                 // IETF RFC-4195
+	"uci",                 // IETF RFC-4179
+	"ucode",               // IETF RFC-6588 ; IETF RFC Errata 3188 ; IETF RFC Errata 3189
+	"uic",                 // Union International de Chemin de Fer
+	"uuid",                // IETF RFC-9562
+	"web3d",               // IETF RFC-3541
+	"wmo",                 // World Meteorological Organization
+	"xmlorg",              // IETF RFC-3120
+	"xmpp",                // IETF RFC-4854
+}
